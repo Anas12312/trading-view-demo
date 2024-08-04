cc6afef4bddb02804a204d96a82ef717d89df3bc
anas12312 <anashesham000@gmail.com>
2024-08-04 13:35:45 +0300

    changed the script to be more configurable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const dotenv = require('dotenv');
 const { timeout } = require('puppeteer');
+const config = require('./config');
 dotenv.config();
 
 function delay(time) {
@@ -119,204 +120,193 @@ async function run(stockName) {
 // run('AAPL')
 
 const runIndcator = async () => {
-    const browser = await puppeteer.launch({
-        timeout: 50_000,
-        headless: false,
-        userDataDir: process.env.CHROME_PATH,
-        args: [
-            "--profile-directory=Profile 1"
-        ]
-    })
+    async function init(headless) {
+        try {
+            console.log("Initializing Browser...")
+            const browser = await puppeteer.launch({
+                timeout: 50_000,
+                headless, // SF to enable headless feature of the browser
+                userDataDir: process.env.CHROME_PATH,
+                args: [
+                    "--profile-directory=Profile 1"
+                ]
+            })
 
-    const page = await browser.newPage()
-    await page.setViewport({
-        height: 900,
-        width: 1500
-    })
-    await page.goto("https://www.tradingview.com/chart/", {
-        waitUntil: "domcontentloaded"
-    })
+            const page = await browser.newPage()
+            await page.setViewport({
+                height: 900,
+                width: 1500
+            })
+            await page.goto("https://www.tradingview.com/chart/", {
+                waitUntil: "domcontentloaded"
+            })
+            console.log("Browser Initialized")
+            return { browser, page }
+        } catch (e) {
+            // console.log("Failed to Initialize the browser, trying again...")
+            await delay(1000)
+            throw new Error("Failed to Initialize the browser, trying again...")
+        }
+    }
+    // Change Ticker Name
+
+    async function changeTicker(stockName, browser) {
+        try {
+            const page = await browser.newPage()
+            await page.setViewport({
+                height: 900,
+                width: 1500
+            })
+            await page.goto("https://www.tradingview.com/chart/", {
+                waitUntil: "domcontentloaded"
+            })
+            console.log("changing the ticker name")
+            await page.waitForSelector('#header-toolbar-symbol-search')
+            const searchBtn = await page.$('#header-toolbar-symbol-search')
+            await searchBtn.click({
+                delay: 10
+            })
+            await page.waitForSelector('[data-name="symbol-search-items-dialog"] input')
+
+            const searchInput = await page.$('[data-name="symbol-search-items-dialog"] input')
+            await delay(200)
+            await searchInput.type(stockName, { delay: 100 });
+            await page.waitForSelector('.scrollContainer-dlewR1s1 .listContainer-dlewR1s1 .itemRow-oRSs8UQo')
+            await delay(500)
+            await page.click('.itemRow-oRSs8UQo div:nth-child(1)')
+            console.log("changed ticker name")
+            return page
+        } catch (e) {
+            console.log("failed to change ticker name, trying again...")
+            return await changeTicker(stockName)
+        }
+    }
 
     // Add Indcator
-
-
-    async function selectIndicator() {
-        console.log("trying to select indication")
+    async function addIndicator(indicatorSearch, indicatorName, browser, page) {
         try {
+            console.log("Adding indicator " + indicatorSearch)
             const indcatorsBtn = await page.waitForSelector('#header-toolbar-indicators button');
-
+            await delay(500)
             await indcatorsBtn.click({
                 delay: 100
             });
-            await page.waitForSelector('.input-qm7Rg5MB');
+
+            await page.waitForSelector('[data-name="indicators-dialog"] input');
             await delay(500)
-            await page.type('.input-qm7Rg5MB', 'ADX')
+            await page.type('[data-name="indicators-dialog"] input', indicatorSearch)
             await delay(100)
-            await page.waitForSelector('[data-title="Average Directional Index"]')
-            await delay(1000)
-
-            await page.click(`[data-title="Average Directional Index"]`, {
+            const template = `[data-title="${indicatorName}"]`
+            await page.waitForSelector(template)
+            await delay(500)
+            await page.click(template, {
                 delay: 100
-            })
+            }) // SF: insepactor value "title" of the indicator
+            await delay(200)
             console.log("clicked on the indicator")
+            try {
+                const exitBtn = await page.waitForSelector('[data-name="indicators-dialog"] button');
 
+                await exitBtn.click({
+                    delay: 100
+                });
+                console.log("closed the dialog")
+            } catch (e) {
+                console.log("couldn't close the dialog")
+                throw new Error()
+            }
         }
         catch (e) {
             console.log(e)
-            await selectIndicator()
+            await delay(200)
+            await addIndicator(indicatorSearch, indicatorName, browser, page)
         }
     }
-    await selectIndicator()
-    await delay(1000)
-    try {
-        const exitBtn = await page.waitForSelector('[data-name="indicators-dialog"] button');
-
-        await exitBtn.click({
-            delay: 100
-        });
-        console.log("closed the dialog")
-    } catch (e) {
-        console.log("couldn't close the dialog")
+    async function addIndicatorIfNotExist(indicatorShort, indicatorName, browser, page) {
+        try {
+            console.log("checking if " + indicatorName + " exists")
+            await page.waitForSelector("[data-name='legend-source-title']")
+            await delay(500)
+            const charts = await page.$$("[data-name='legend-source-title']")
+            let exists = false
+            for (let chart of charts) {
+                const chartLegend = await chart.evaluate(c => c.textContent)
+                if (chartLegend == indicatorShort) {
+                    console.log(indicatorShort + " already exists")
+                    exists = true
+                    break
+                }
+            }
+            if (!exists) await addIndicator(indicatorShort, indicatorName, browser, page)
+        } catch (e) {
+            console.log("failed to check which indicators exist, trying again...")
+            await addIndicatorIfNotExist(indicatorShort, indicatorName, browser, page)
+        }
     }
+    async function addAllIndicators(browser, page) {
+        const indicators = config.indicators
+        for (let indicator of indicators) {
+            await addIndicatorIfNotExist(indicator.short, indicator.name, browser, page)
+        }
+    }
+    // await delay(1000) // SF: to remvoe the delay
+
     // Save Changes
-    await delay(1000)
-    async function save() {
+    // await delay(1000) // SF: to remvoe the delay
+    async function save(browser, page) {
         try {
             await page.click('button#header-toolbar-save-load', {
                 delay: 1000
             })
             console.log("saved")
         } catch (e) {
+            await delay(200)
             console.log("failed to save, trying again...")
-            await save()
+            await save(browser, page)
         }
     }
-    await save()
-    // Select Indcator from Chart
 
-    async function selectTheIndicatorLegend() {
+
+    // create alert
+    async function createAlert(charts, index, browser, page) {
         try {
-            await page.waitForSelector("[data-name='legend-source-title']");
-            await delay(500)
-            const charts = await page.$$("[data-name='legend-source-title']")
+            await delay(200)
+            const indcatorLegend = charts.at(index);
+            await indcatorLegend?.click();
+            await delay(200)
+            await page.keyboard.down('Alt')
+            await page.keyboard.down('A')
 
-            const indcatorLegend = charts.at(1);
-            await indcatorLegend.click();
-            console.log("selected the indicator legend")
+            await page.keyboard.up('Alt')
+            await page.keyboard.up('A')
+            const createAlertBtn = await page.waitForSelector('button[data-name="submit"]', {
+                timeout: 1000
+            });
+            await createAlertBtn.click({
+                delay: 100
+            });
+            console.log("alert created")
         } catch (e) {
-            console.log("failed to select the indicator legend")
-            await selectTheIndicatorLegend()
+            console.log(e)
+            console.log("failed to create alert, trying again...")
+            await delay(50)
+            await createAlert(charts, index, browser, page)
         }
     }
-    await delay(1000)
-    await selectTheIndicatorLegend()
-
-    await delay(1000);
-
-    // Setting Button
-    try {
-        await page.waitForSelector('[data-name="legend-settings-action"]')
-        const indcatorSettingButton = (await page.$$('[data-name="legend-settings-action"]')).at(0)
-        await indcatorSettingButton.click();
-        console.log("opened the settings")
-    } catch (e) {
-        console.log("failed to open the settings")
-    }
-
-    try {
-        await delay(1000)
-        await page.waitForSelector('[data-name="indicator-properties-dialog"] input')
-        const smoothingInput = (await page.$$('[data-name="indicator-properties-dialog"] input')).at(0)
-
-        await smoothingInput.type('25', {
-            delay: 100
-        });
-
-        // await delay(1000);
-
-        const DI_Length = (await page.$$('[data-name="indicator-properties-dialog"] input')).at(1)
-
-        await DI_Length.type('13', {
-            delay: 100
-        });
-        console.log("changed settings")
-    } catch (e) {
-        console.log("failed to change the settings")
-    }
-
-    // await delay(1000);
-    try {
-        await delay(1000)
-        const okBtn = await page.waitForSelector('button[name="submit"]')
-        await okBtn.click();
-        console.log("clicked submit settings")
-    } catch (e) {
-        console.log("failed to click submit settings")
-    }
-
-    await save()
-
     // More Action Button
-    await page.waitForSelector("[data-name='legend-source-title']");
-    await delay(500)
-    const charts = await page.$$("[data-name='legend-source-title']")
-
-    const indcatorLegend = charts.at(1);
-    await indcatorLegend?.click();
-
-    await page.keyboard.down('Alt')
-    await page.keyboard.down('A')
-
-    await page.keyboard.up('Alt')
-    await page.keyboard.up('A')
-
-    // Notification Tab
-    try {
-        await delay(1000)
-        const notificationTabBtn = await page.waitForSelector('#alert-dialog-tabs__notifications');
-
-        await notificationTabBtn.click({
-            delay: 10
-        })
-        console.log("pressed on the notification tab")
-    } catch (e) {
-        console.log("failed to press on the notification tab")
+    async function addingAlerts(browser, page) {
+        await page.waitForSelector("[data-name='legend-source-title']");
+        await delay(500)
+        const charts = await page.$$("[data-name='legend-source-title']")
+        for (let i in charts) {
+            if (i == 0) continue
+            await createAlert(charts, i, browser, page)
+            await delay(500)
+        }
     }
 
-    // // const enableWebhook = await page.waitForSelector('div[data-name="alerts-create-edit-dialog"] .tabsWrapper-v6smTDmN div[data-name="underline-tabs-buttons"] div#id_alerts-create-edit-dialog-tabs_tablist button#alert-dialog-tabs__notifications');
-    // // await notificationTabBtn.click({
-    // //     delay: 10
-    // // })
 
-    // // await page.click('#alert-dialog-tabs__notifications',
-    // //     { delay: 1000 }
-    // // )
-
-    // // await page.click('input[data-name="webhook"]',
-    // //     { delay: 100 }
-    // // );
-
-    // // await page.type('input#webhook-url',
-    // //     'http://18.220.204.73/webhook',
-    // //     { delay: 50 }
-    // // );
-
-    try {
-        await delay(1000)
-        const createAlertBtn = await page.waitForSelector('button[data-name="submit"]');
-        await delay(1000)
-        await createAlertBtn.click({
-            delay: 100
-        });
-        console.log("pressed create alert")
-    } catch (e) {
-        console.log("failed to press create alert")
-    }
-
-    // Save Changes
-    await save()
-    await delay(1000)
-    async function zoomOut() {
+    async function zoomOut(browser, page) {
         await page.keyboard.down('Control')
         await page.keyboard.down('ArrowDown')
         await page.keyboard.up('ArrowDown')
@@ -326,35 +316,40 @@ const runIndcator = async () => {
         await delay(500)
         await page.keyboard.down('ArrowDown')
         await page.keyboard.up('ArrowDown')
+        await delay(500)
+        await page.keyboard.down('ArrowDown')
+        await page.keyboard.up('ArrowDown')
+        await delay(500)  // SF: to increase the zoomout before savng the .csv  .. repeate the below 3 lines
+        await page.keyboard.down('ArrowDown')
+        await page.keyboard.up('ArrowDown')
     }
-    await zoomOut()
-    async function downloadCSV() {
+    async function downloadCSV(browser, page) {
         try {
             await page.waitForSelector('[data-name="save-load-menu"]')
             const dropDownBtn = await page.$('[data-name="save-load-menu"]')
             await dropDownBtn.click({
                 delay: 20
             })
-            await delay(1000)
+            // await delay(1000) // SF: to remvoe the delay
             console.log("clicked on the dropdown button")
             await page.waitForSelector('[data-name="menu-inner"] [data-role="menuitem"]')
             const menuBtn = (await page.$$('[data-name="menu-inner"] [data-role="menuitem"]')).at(5)
             menuBtn.click({
                 delay: 100
             })
-            await delay(1000)
+            // await delay(1000) // SF: to remvoe the delay
             console.log("clicked on the menu button")
             await page.waitForSelector("#time-format-select")
             const dropDownButton = (await page.$("#time-format-select"))
             await dropDownButton.click({
                 delay: 20
             })
-            await delay(1000)
+            // await delay(1000) // SF: to remvoe the delay
             await page.waitForSelector("#time-format-iso")
             await page.click("#time-format-iso", {
                 delay: 20
             })
-            await delay(1000)
+            // await delay(1000) // SF: to remvoe the delay
             console.log("changed time format")
             await page.click("[data-name='submit-button']", {
                 delay: 20
@@ -362,12 +357,37 @@ const runIndcator = async () => {
             console.log("submitted the downloadCSV dialog")
         } catch (e) {
             console.log("failed to save CSV, trying again...")
-            await downloadCSV()
+            await downloadCSV(browser, page)
         }
     }
-    await downloadCSV()
-
+    // await downloadCSV()
+    // await delay(3000)
+    // await browser.close()
+    // async function handleTicker(ticker) {
+    //     const newPage = await changeTicker(ticker, browser, page)
+    //     await addingAlerts(browser, newPage)
+    //     await save(browser, newPage)
+    //     await zoomOut(browser, newPage)
+    // }
+    const { browser, page } = await init(false)
+    await addAllIndicators(browser, page)
+    await save(browser, page)
+    for (let ticker of config.tickers) {
+        const newPage = await changeTicker(ticker, browser, page)
+        await addingAlerts(browser, newPage)
+        await save(browser, newPage)
+        await zoomOut(browser, newPage)
+        // await downloadCSV(browser, newPage)
+        // await delay(500)
+    }
 
 }
 
 runIndcator()
+// async function main() {
+//     const tickers = ['TSLA', 'AAPL', 'AMZN']
+//     for (let ticker of tickers) {
+//         await runIndcator(ticker)
+//     }
+// }
+// main()
